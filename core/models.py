4cfcfa9960f49dee74a588a6d0ada701288da513
Lucas Berenguer <lucasberenguers4@gmail.com>
2026-03-30 10:15:06 -0300

    vitrines definidas - smartmall

--- core/models.py
+++ core/models.py
@@ -4,11 +4,11 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
 class Loja(models.Model):
     nome = models.CharField(max_length=100)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.name
+        return self.nome
